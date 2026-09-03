fdaf9652cb54b504a5523379d519ccd608d578ad
ZihaoHoo <5214194+ZihaoHoo@users.noreply.github.com>
2026-09-03 17:54:21 +0800

    Add Wk1 Act 3

--- wk1/Week_1_Lab.py
+++ wk1/Week_1_Lab.py
@@ -1,25 +1,44 @@
 # HOO ZI HAO (2603028)
 
+#region Global Variables
+Act1Msg = "What am I doing in this course?"
+username = ""
+bio = ""
+followers = 0
+age = 0
+category = ""
+#endregion
+
 #region Activity 1
 def print_Act1(message, char="="):
     divider = char * len(message)
     print(f"{divider}\n{message}\n{divider}")
-
-Act1Msg = "What am I doing in this course?"
-#print_Act1(Act1Msg)
 #endregion 
 
 #region Activity 2
 def hardcodedAct2():
+    global username, bio, followers
     username ="John Doe"
     bio = "I am a software developer with a passion for learning new technologies."
     followers = 150
 
     info = [
-    f"Username: {username}",
-    f"Bio: {bio}",
-    f"Followers: {followers}"]
+        f"Username: {username}",
+        f"Bio: {bio}",
+        f"Followers: {followers}"]
     print("\n".join(info))
+#endregion
 
-hardcodedAct2()
+#region Activity 3
+def FollowerGrowthAct3():
+    followers= 100
+
+    followers += 50
+    print("Day 1: Followers =", followers)
+
+    followers += 20
+    print("Day 2: Followers =", followers) 
+
+    followers -= 10
+    print("Day 3: Followers =", followers)
 #endregion
