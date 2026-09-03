68ff7fef444dc61b8f5b50f24e8ab5881bbafabb
ZihaoHoo <5214194+ZihaoHoo@users.noreply.github.com>
2026-09-03 17:33:21 +0800

    Add Wk1 Act 2

--- wk1/Week_1_Lab.py
+++ wk1/Week_1_Lab.py
@@ -1,10 +1,25 @@
 # HOO ZI HAO (2603028)
 
-# Activity 1
+#region Activity 1
 def print_Act1(message, char="="):
     divider = char * len(message)
     print(f"{divider}\n{message}\n{divider}")
 
 Act1Msg = "What am I doing in this course?"
-print_Act1(Act1Msg)
+#print_Act1(Act1Msg)
+#endregion 
 
+#region Activity 2
+def hardcodedAct2():
+    username ="John Doe"
+    bio = "I am a software developer with a passion for learning new technologies."
+    followers = 150
+
+    info = [
+    f"Username: {username}",
+    f"Bio: {bio}",
+    f"Followers: {followers}"]
+    print("\n".join(info))
+
+hardcodedAct2()
+#endregion
