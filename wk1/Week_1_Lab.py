7dcceaf7ac7d093deec33ce9c740b264a5da052c
ZihaoHoo <5214194+ZihaoHoo@users.noreply.github.com>
2026-09-03 17:21:35 +0800

    Add Wk1 Act 1

---
+++ wk1/Week_1_Lab.py
@@ -0,0 +1,10 @@
+# HOO ZI HAO (2603028)
+
+# Activity 1
+def print_Act1(message, char="="):
+    divider = char * len(message)
+    print(f"{divider}\n{message}\n{divider}")
+
+Act1Msg = "What am I doing in this course?"
+print_Act1(Act1Msg)
+
